51276d97e2b20a2942ab2672a21c6ae1bec707a4
Andras Karasz <karaszandris@gmail.com>
2026-04-18 13:55:38 +0200

    nvim open files externally from explorer

--- .config/nvim/lua/plugins/snacks.lua
+++ .config/nvim/lua/plugins/snacks.lua
@@ -1,8 +1,35 @@
 return {
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = { enabled = false },
+  "folke/snacks.nvim",
+  opts = {
+    dashboard = { enabled = false },
+
+    explorer = {
+      win = {
+        list = {
+          keys = {
+            ["O"] = "open_external",
+          },
+        },
+      },
+      actions = {
+        open_external = function(_, item)
+          local path = item.file
+          if not path then
+            return
+          end
+
+          local cmd
+          if vim.fn.has("win32") == 1 then
+            cmd = { "explorer" }
+          elseif vim.fn.has("mac") == 1 then
+            cmd = { "open" }
+          else
+            cmd = { "xdg-open" }
+          end
+
+          vim.fn.jobstart(vim.list_extend(cmd, { path }), { detach = true })
+        end,
+      },
     },
   },
 }
